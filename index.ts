25cee4b07ea89b359946cf9890c151d974bb6fd9
lam.nguyenquoc1 <lam.nguyenquoc1@V004406>
2025-10-06 15:40:10 +0700

    update

--- index.ts
+++ index.ts
@@ -8,13 +8,12 @@ app.start(async () => {
   const http = new Elysia()
 
   http.use(swagger())
-  // http.use(cors({
-  //   origin: app.service.config.URL_CLIENT
-  // }))
+  http.use(cors({
+    origin: app.service.config.URL_CLIENT
+  }))
   http.use(routers)
   http.listen({
     port: Number(app.service.config.PORT) || 3000,
-    hostname: '0.0.0.0',
     idleTimeout: 80, // Đặt timeout thành 30 giây
   })
   app.on('stop', () => http.stop())
